b7b86b08d4d62bb3d605517aeef06f4efcf11362
Anderson SIlva <somdesp@gmail.com>
2018-12-14 14:10:12 -0200

    Implementando Upload de Arquivos

---
+++ PFC/App/Controller/FileUploadController.js
@@ -0,0 +1,16 @@
+﻿
+MeHelp.controller('FileUploadController', function ($scope, fileUploadService) {
+
+    $scope.uploadFile = function () {
+        var file = $scope.myFile;
+        var uploadUrl = "../server/service.php", //Url of webservice/api/server
+            promise = fileUploadService.uploadFileToUrl(file, uploadUrl);
+
+        promise.then(function (response) {
+            $scope.serverResponse = response;
+        }, function () {
+            $scope.serverResponse = 'An error has occurred';
+        })
+    };
+});
+
--- PFC/App/Controller/topicoSelecionado.controller.js
+++ PFC/App/Controller/topicoSelecionado.controller.js
@@ -107,6 +107,20 @@
         return DadosTopico;
     };
 
+    $scope.uploadFile = function () {
+        var file = $scope.myFile;
+        var uploadUrl = "../server/service.php", //Url of webservice/api/server
+            promise = fileUploadService.uploadFileToUrl(file, uploadUrl);
+
+        promise.then(function (response) {
+            $scope.serverResponse = response;
+        }, function () {
+            $scope.serverResponse = 'An error has occurred';
+        })
+    };
+
+
+
     //Listar Temas
     function carregarTemas() {
         var listarTemas = topicoService.getTodosTemas();
